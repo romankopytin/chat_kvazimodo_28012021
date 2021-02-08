01f16617a46cc2b1cdd49cf6fc86ca5dbf3942f3
Roman <kopytin.roman@gmail.com>
2021-02-08 17:17:57 +0300

    DZ

--- server/src/main/java/server/AuthService.java
+++ server/src/main/java/server/AuthService.java
@@ -2,4 +2,6 @@ package server;
 
 public interface AuthService {
     String getNicknameByLoginAndPassword (String login, String password); // Метод принимает логин и пароль, в ответ возвращает никнейм
+
+    boolean registration (String login, String password, String nickname);
 }
